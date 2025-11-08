ab3fdabf89f01b9273c31adc4367f9c910d53cd1
unknown <ragavixyz@gmail.com>
2025-11-08 17:25:10 +0530

    Added test route

--- DreamNex/backend/src/app.js
+++ DreamNex/backend/src/app.js
@@ -33,5 +33,7 @@ mongoose.connect(process.env.MONGODB_URI, {
 })
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
-
+app.get('/', (req, res) => {
+  res.send('DreamNex backend is working!');
+});
 module.exports = app;
